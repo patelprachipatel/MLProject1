c3bf2b926f252b7920f53ff42bcdf0bb075b81d4
patelprachipatel <prachi.patel.business@gmail.com>
2024-11-26 23:53:34 +0530

    first set up

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    get the list of all the libraries in requirements
+    '''
+    requirements = []
+    
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n',"") for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+
+    return requirements 
+
+
+
+setup(
+name='MLProject1',
+version='0.0.1',
+author='PrachiPatel',
+author_email='prachi.patel.business@gmail.com',
+packages=find_packages(),
+install_requires= get_requirements('requirements.txt')
+)
