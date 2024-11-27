b3958397b746e5d37cdaa439a415f711a1d8bd5c
patelprachipatel <prachi.patel.business@gmail.com>
2024-11-28 00:28:00 +0530

    file template created

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_requirements(file_path:str) -> List[str]:
 
 
 setup(
-name='MLProject1',
+name='mlproject1',
 version='0.0.1',
 author='PrachiPatel',
 author_email='prachi.patel.business@gmail.com',
